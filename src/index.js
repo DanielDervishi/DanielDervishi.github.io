b5c98a01bd9405c445e790ada7698b1b87b997f4
Daniel Dervishi <d.dervishi@mail.utoronto.ca>
2023-01-02 11:54:46 -0500

    Changed routing hook to work on github pages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Particles from 'particlesjs';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 console.log("got here")
 const root = ReactDOM.createRoot(document.getElementById('root'));
